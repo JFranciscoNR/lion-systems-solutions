cf5e45609ec6143cc8edbe9d25fbd9545c080c8b
JFranciscoNR <josefrancisconavarodriguez0405@gmail.com>
2022-10-03 17:42:16 -0500

    Agregamos la ruta salas en el dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,9 @@
                                     <a class="dropdown-item" href="{{ route('estatus.index') }}">
                                         {{ __('Estatus') }}
                                     </a>
+                                    <a class="dropdown-item" href="{{ route('salas.index') }}">
+                                        {{ __('Salas') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
