287926540b1cde27c00067d7b3e1cd9e472d043d
JFranciscoNR <josefrancisconavarodriguez0405@gmail.com>
2022-10-03 15:47:27 -0500

    Creación de rutas necesarias para crud de estatus

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,19 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Creación de las rutas necesarias para realizar nuestro crud de estatus
+//Ruta que muestra el listado de registros
+Route::get('estatus', [EstatuController::class, 'index'])->name('estatus.index');
+//Ruta de redireccionamiento a la vista para crear un nuevo registro
+Route::get('estatus/create', [EstatuController::class, 'create'])->name('estatus.create');
+//Ruta para crear un nuevo registro
+Route::post('estatus', [EstatuController::class, 'store'])->name('estatus.store');
+//Ruta que muestra los detalles de cada registro
+Route::get('estatus/{estatu}', [EstatuController::class, 'show'])->name('estatus.show');
+//Ruta de redireccionamiento a la vista para actualizar un registro
+Route::get('estatus/{estatu}/edit', [EstatuController::class, 'edit'])->name('estatus.edit');
+//Ruta para actualizar un regustro
+Route::put('estatus/{estatu}', [EstatuController::class, 'update'])->name('estatus.update');
+//Ruta para eliminar un registro
+Route::delete('estatus/{estatu}/delete', [EstatuController::class, 'destroy'])->name('estatus.destroy');
