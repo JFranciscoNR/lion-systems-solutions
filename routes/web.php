e14df75ef3c61df47ac61ee6c3133af9847cc587
JFranciscoNR <josefrancisconavarodriguez0405@gmail.com>
2022-10-03 17:40:03 -0500

    Creación de rutas necesarias para crud de salas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EstatuController;
+use App\Http\Controllers\SalaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,3 +36,19 @@ Route::get('estatus/{estatu}/edit', [EstatuController::class, 'edit'])->name('es
 Route::put('estatus/{estatu}', [EstatuController::class, 'update'])->name('estatus.update');
 //Ruta para eliminar un registro
 Route::delete('estatus/{estatu}/delete', [EstatuController::class, 'destroy'])->name('estatus.destroy');
+
+//Creación de las rutas necesarias para realizar nuestro crud de salas
+//Ruta que muestra el listado de registros
+Route::get('salas', [SalaController::class, 'index'])->name('salas.index');
+//Ruta de redireccionamiento a la vista para crear un nuevo registro
+Route::get('salas/create', [SalaController::class, 'create'])->name('salas.create');
+//Ruta para crear un nuevo registro
+Route::post('salas', [SalaController::class, 'store'])->name('salas.store');
+//Ruta que muestra los detalles de cada registro
+Route::get('salas/{sala}', [SalaController::class, 'show'])->name('salas.show');
+//Ruta de redireccionamiento a la vista para actualizar un registro
+Route::get('salas/{sala}/edit', [SalaController::class, 'edit'])->name('salas.edit');
+//Ruta para actualizar un regustro
+Route::put('salas/{sala}', [SalaController::class, 'update'])->name('salas.update');
+//Ruta para eliminar un registro
+Route::delete('salas/{sala}/delete', [SalaController::class, 'destroy'])->name('salas.destroy');
